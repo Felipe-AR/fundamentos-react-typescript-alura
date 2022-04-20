56785501d8b05a0aec584e51dd1fe382aff01282
Felipe-AR <fefeucn_cm@hotmail.com>
2022-04-20 20:55:51 -0300

    Alteração para tornar o botão dinâmico

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import style from "./botao.module.scss";
 
-class Botao extends React.Component<{ children: string }> {
+class Botao extends React.Component<{
+  type?: "button" | "submit" | "reset" | undefined;
+  children: string;
+}> {
   render() {
-    return <button className={style.botao}>{this.props.children}</button>;
+    const { type = "button" } = this.props;
+    return (
+      <button type={type} className={style.botao}>
+        {this.props.children}
+      </button>
+    );
   }
 }
 
